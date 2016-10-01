4314f6ec3a3df008bd4d92f18c4631793259f26d
GlebKalinichenko <Glebjn@yandex.ua>
2016-10-01 21:07:41 +0400

    Add comment's panel on details screen (reverted from commit 1595de5ee674c3649d450796612430d6c55d2210)

---
+++ app/src/main/java/com/example/gleb/mediumme/PostListBottomSheetCallback.kt
@@ -0,0 +1,8 @@
+package com.example.gleb.mediumme
+
+import android.support.design.widget.BottomSheetBehavior
+import android.view.View
+
+open class PostListBottomSheetCallback : BottomSheetBehavior.BottomSheetCallback {
+
+}
